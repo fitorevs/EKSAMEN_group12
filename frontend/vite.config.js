b3390059865082a07e1ef6c8559d0b58482a350d
NL-12 <nikol.lysebraate@hiof.no>
2024-05-21 00:11:13 +0200

    Lagt til riktig versjon as vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SANITY_USER_TOKEN': JSON.stringify(env.SANITY_USER_TOKEN),
+      'process.env.SANITY_PROJECT_ID': JSON.stringify(env.SANITY_PROJECT_ID),
+      'process.env.X_RAPIDAPI_KEY': JSON.stringify(env.X_RAPIDAPI_KEY),
+      'process.env.X_RAPIDAPI_HOST': JSON.stringify(env.X_RAPIDAPI_HOST)
+    },
+    plugins: [react()],
+  }
 })
