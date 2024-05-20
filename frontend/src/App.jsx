0b2d4eef39fb13d06cb97362b08988f49f324355
NL-12 <nikol.lysebraate@hiof.no>
2024-05-21 00:06:45 +0200

    La til riktig App.jsx og riktig main.jsx
    Samt å fjerne overflødige filer.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* Dette er hoved funksjonen. Den blir kalt av main.jsx og er ansvarlig for å sette opp dynamisk routing. */
 
-function App() {
-  const [count, setCount] = useState(0)
+/* Linje 4: Hent BuildRoutes funksjonen fra components. */
+import BuildRoutes from './components/BuildRoutes'
+/* Linje 6: Last in den sentrale SASS filen. */
+import './styles/Main.scss'
 
+/* Linje 9 til 15: Standard oppsett. Kall BuildRoutes for å sette opp alt. */
+function App() {  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BuildRoutes />
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
+/* Linje 2 til 4: Standard import. */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+/* Linje 6: Laste inn normalize for å "nullstille" all CSS. */
+import './normalize.css'
+/* Linje 8: Hente in BrowserRouter fra react-router-dom for å sette opp global routing. */
+import { BrowserRouter as Router } from 'react-router-dom'
 
+/* Linje 11 til 15: Wrap kallet til App funksjonen i en routing instans. */
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 )
